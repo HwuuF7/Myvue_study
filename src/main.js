652fdce0e4b27465f272cc64ee4f6a23ac8441f1
HwuuF7 <914334593@qq.com>
2020-03-29 14:53:32 +0800

    完成用户列表

--- src/main.js
+++ src/main.js
@@ -10,6 +10,10 @@ import './assets/fonts/iconfont.css'
 Vue.config.productionTip = false
 // 配置根路径
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 new Vue({
