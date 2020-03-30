ff50c3f58de563cd14cf1dfa1a965933ba9a7a81
HwuuF7 <914334593@qq.com>
2020-03-30 19:30:51 +0800

    完成了分类

--- src/main.js
+++ src/main.js
@@ -6,8 +6,10 @@ import axios from 'axios'
 import './plugins/element.js'
 import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
+import TreeTable from 'vue-table-with-tree-grid'
 
 Vue.config.productionTip = false
+Vue.component('tree-table', TreeTable)
 // 配置根路径
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
