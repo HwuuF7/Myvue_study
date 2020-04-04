d7bbe991ac78df29aedf70ce57e1cd9c9bb2bdaf
HwuuF7 <914334593@qq.com>
2020-04-04 22:19:22 +0800

    完成订单

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -36,7 +36,9 @@ import {
   Steps,
   Checkbox,
   CheckboxGroup,
-  Upload
+  Upload,
+  Timeline,
+  TimelineItem
 } from 'element-ui'
 
 Vue.use(Button)
@@ -74,6 +76,8 @@ Vue.use(Steps)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 // 全局挂载 message可自定义
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
